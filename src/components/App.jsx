bfb1fd839e617f218feef91da00f70f06816768d
brizhak <ada.adelina17@gmail.com>
2023-07-12 14:43:28 +0300

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import user from './json/user.json';
+import Profile from './Profile';
+
+const App = () => {
+    return (
+        <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+  )};
+
+export default App;
