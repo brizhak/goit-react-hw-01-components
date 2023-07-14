bbf6ae9b762cab9bd22619996eb6ee3d79e7aa80
brizhak <ada.adelina17@gmail.com>
2023-07-14 15:04:27 +0300

    update

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -34,8 +34,8 @@ Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  avatar: PropTypes.string,
-  stats: PropTypes.object,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
 };
 
 export default Profile;
